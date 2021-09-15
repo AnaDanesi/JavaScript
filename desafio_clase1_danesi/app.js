58a35ebdfd7fe49ffa231ac85995c5c0df060bec
Anabella Danesi <89553737+AnaDanesi@users.noreply.github.com>
2021-09-14 21:42:58 -0300

    Add files via upload

---
+++ desafio_clase1_danesi/app.js
@@ -0,0 +1,22 @@
+let nombre = prompt('ingresa tu nombre');
+let apellido = prompt('ingresa tu apellido');
+
+console.log(nombre);
+console.log(apellido);
+
+console.log(nombre + "  " + apellido);
+
+
+
+var numero1 = 2;
+let numero2 = 5;
+
+let resultadoSuma = numero1 + numero2;
+
+
+
+
+
+
+
+
